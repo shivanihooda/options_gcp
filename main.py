741db6069bd3b38b9fa293a195603206a29b79a9
unknown <ec22174@qmul.ac.uk>
2023-07-12 21:39:56 +0100

    working code

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from pandas.io import gbq
 import pandas_gbq
 from datetime import datetime
 import yahoo_fin.options as ops
-import google.cloud.logging
-import logging
+from google.cloud import logging
 from google.cloud import bigquery
+import os
 
-
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='C:\\Users\\MNC\\Desktop\\options_test\\polished-parser-390314-7be98befde9a.json'
 # import yahoo_fin.stock_info as stocks
 
 # ticker_list = {"AAPL", "MSFT", "GOOG", "AMZN", "NVDA", "TSLA", "BRK-B", "META", "V", "UNH", "LLY",
@@ -18,15 +18,14 @@ from google.cloud import bigquery
 #                "COST", "ABBV", "BAC", "ADBE", "MCD", "CSCO", "PFE", "CRM", "ACN", "TMO", "NFLX", "ABT",
 #                "LIN", "AMD", "DHR", "CMCSA", "NKE", "TXN", "DIS", "WFC", "VZ", "UPS", "PM", "NEE", "MS", "INTC"}
 
-ticker_list = ["AAPL", "MSFT"]
+ticker_list = ["AAPL"]
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-log_client = google.cloud.logging.Client()
-log_client.setup_logging()
+log_client = logging.Client()
+log_name = "cloud-funtion-log"
+logging = log_client.logger(log_name)
 
 
-def options_data_sync():
+def main():
     #   request.json = request.get_json()
 
     #   if request.args:
@@ -40,7 +39,7 @@ def options_data_sync():
     #   else:
     # logging.basicConfig(level=logging.DEBUG)  # Set logging level to DEBUG
 
-    logging.info("This is an info message.")
+    logging.log_text("This is an info message.")
 
     get_options_data()
     return f'Data pull complete'
@@ -63,14 +62,13 @@ def get_options_data():
                     "ask": json_d["Ask"],
                     "change": json_d["Change"],
                     "percent_change": json_d["% Change"],
-                    "volume": json_d["Volume"],
+                    # "volume": json_d["Volume"],
                     "open_interest": json_d["Open Interest"],
                     "implied_volatility": json_d["Implied Volatility"],
                     "option_type": "Call",
                     "timestamp": datetime.now(pytz.timezone('Europe/London')).strftime("%Y:%m:%d %H:%M:%S")
                 }
                 final_json.append(revised_json)
-            logging.info(f"calls={final_json}")
             data = ops.get_puts(ticker)
             json_data = json.loads(data.to_json(orient="records"))
             for json_d in json_data:
@@ -83,7 +81,7 @@ def get_options_data():
                     "ask": json_d["Ask"],
                     "change": json_d["Change"],
                     "percent_change": json_d["% Change"],
-                    "volume": json_d["Volume"],
+                    # "volume": json_d["Volume"],
                     "open_interest": json_d["Open Interest"],
                     "implied_volatility": json_d["Implied Volatility"],
                     "option_type": "Put",
@@ -91,27 +89,27 @@ def get_options_data():
                 }
                 final_json.append(revised_json)
             count = count + 1
-            logging.info(f"puts:{final_json}")
+            logging.log_text(f"Final json is ready!")
         except Exception as exc:
-            logging.info("Exception Occurred: ", ticker, " : ", exc)
+            logging.log_text("Exception Occurred: ", ticker, " : ", exc)
             continue
     df = pd.DataFrame(final_json)
-    logging.info(f"df data:{df}")
+    temp = pd.to_datetime(df["timestamp"])
+    df["timestamp"] = temp
+    # df = df["volume"].astype(str).astype(int)
+    print(df.dtypes)
+    logging.log_text(f"DF is ready!")
     bq_load('options_data_rt', df)
 
 
 def bq_load(key, value):
-    logging.info(f"Off to BQ")
+    logging.log_text(f"Off to BQ")
     table_id = "polished-parser-390314.options_data.options_data_rt"
     client = bigquery.Client()
     table = client.get_table(table_id)
-    errors = client.insert_rows_from_dataframe(table, value)  # Make an API request.
-    if errors == []:
-        logging.info("Data Loaded")
-        return "Success"
-    else:
-        logging.info(errors)
-        return "Failed"
+    job = client.load_table_from_dataframe(value, table)  # Make an API request.
+    logging.log_text("Waiting for job to finish!")
+    job.result()
     # project_name = 'polished-parser-390314'
     # dataset_name = 'options_data'
     # table_name = key
@@ -121,4 +119,6 @@ def bq_load(key, value):
     # value.to_gbq(destination_table='{}.{}'.format(dataset_name, table_name), project_id=project_name,
     #              if_exists='replace')
 
-    # logging.info(f"DONE DONE DONE")
+    logging.log_text(f"DONE DONE DONE")
+
+main()
